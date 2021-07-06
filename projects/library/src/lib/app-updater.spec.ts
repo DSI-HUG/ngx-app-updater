751d177e27d0513ef0142139276387a2d6d4f0d9
Badisi <badisi84@gmail.com>
2021-07-06 14:19:38 +0200

    fix(test:lib)

---
+++ projects/library/src/lib/app-updater.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+
+import { AppUpdaterModule } from './app-updater.module';
+import { AppUpdaterService } from './services/app-updater.service';
+
+describe('NgxSentryService', () => {
+    let service: AppUpdaterService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterModule.forRoot([]),
+                AppUpdaterModule.forRoot({
+                    enabled: false
+                })
+            ]
+        });
+        service = TestBed.inject(AppUpdaterService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+});
